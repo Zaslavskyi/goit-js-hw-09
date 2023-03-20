0f9864c9b0c347ec0c4b211369c135e33001c5a1
Zaslavskyi <115506935+Zaslavskyi@users.noreply.github.com>
2023-03-20 22:27:14 +0000

    Deploying to gh-pages from @ Zaslavskyi/goit-js-hw-09@c33f007aed54effe620f8733d551948820e52efd 🚀

---
+++ 01-color-switcher.59323164.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");function n(){button.setAttribute("disabled","disabled")}t.addEventListener("click",onStartBtnClick),e.addEventListener("click",onStopBtnClick),n()}();
+//# sourceMappingURL=01-color-switcher.59323164.js.map
---
+++ 01-color-switcher.f7097d91.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");function n(){button.setAttribute("disabled","disabled")}t.addEventListener("click",onStartBtnClick),e.addEventListener("click",onStopBtnClick),n();
+//# sourceMappingURL=01-color-switcher.f7097d91.js.map
