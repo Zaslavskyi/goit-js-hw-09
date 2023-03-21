cecbc5acc238147c25e40001e41a801ed7a0c261
Zaslavskyi <bbaly89@gmail.com>
2023-03-22 01:51:35 +0200

    may be yes, may be no...

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -17,10 +17,10 @@ function onFormSubmit(e) {
   for (let position = 1; position <= amount; position +=1) {
     createPromise({position, delay})
     .then(({position, delay}) => {
-      console.log(`Fulfilled promise ${position} in ${delay}ms`);
+      Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
     })
     .catch(({position, delay}) => {
-      console.log(`Rejected promice ${position} in ${delay}ms`);
+      Notiflix.Notify.failure(`Rejected promice ${position} in ${delay}ms`);
     });
     delay += step;
   };
